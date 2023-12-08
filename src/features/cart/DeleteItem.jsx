fdea8bca2c3ddc3feed550c97a774aa44e738337
jite <jitekobimowe10@gmail.com>
2023-12-08 12:09:01 +0100

    add React Pizza app

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+import Button from "../../ui/Button"
+import { deleteItem } from "./cartSlice"
+
+// eslint-disable-next-line react/prop-types
+function Deleteitem({pizzaId}) {
+    const dispatch = useDispatch();
+
+  return (
+    <Button type="small" onClick={() => dispatch (deleteItem(pizzaId))}>Delete</Button>
+  )
+}
+
+export default Deleteitem
